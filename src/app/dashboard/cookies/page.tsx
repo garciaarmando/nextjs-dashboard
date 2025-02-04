540b7b457ce5501e97df6ee676ecbd2a215d5323
Armando Garcia <armando.garcia@employbridge.com>
2025-02-04 15:20:23 -0600

    clase 155 - cookies server side

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,3 +1,4 @@
+import { cookies } from 'next/headers'
 import { TabBar } from "@/components";
 
 export const metadata = {
@@ -6,11 +7,17 @@ export const metadata = {
 }
 
 export default function CookiesPage() {
+
+const cookieStore = cookies()
+const cookieTab = Number(cookieStore.get('selectedTab')?.value ?? '1')
+
+const allCookies = cookieStore.getAll()
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
+        {JSON.stringify(allCookies)}
         <div className="flex flex-col">
             <span className="text-3xl">Tabs</span>
-            <TabBar/>
+            <TabBar currentTab={cookieTab}/>
         </div>
     </div>
   );
