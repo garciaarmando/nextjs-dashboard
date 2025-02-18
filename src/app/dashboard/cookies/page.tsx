d7f25c14c6af78aa4e87e14adab263aa4af11621
Armando Garcia <armandobfmv@gmail.com>
2025-02-18 12:57:58 -0600

    small change

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -14,7 +14,6 @@ const cookieTab = Number(cookieStore.get('selectedTab')?.value ?? '1')
 const allCookies = cookieStore.getAll()
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
-        {JSON.stringify(allCookies)}
         <div className="flex flex-col">
             <span className="text-3xl">Tabs</span>
             <TabBar currentTab={cookieTab}/>
--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -2,6 +2,7 @@
 'use client'
 
 import { setCookie } from "cookies-next"
+import { useRouter } from "next/navigation"
 import { useState } from "react"
 
 interface Props {
@@ -10,10 +11,12 @@ interface Props {
 }
 
 export const TabBar = ({ currentTab = 1, tabOptions = [1, 2, 3, 4] }: Props) => {
+    const router = useRouter()
     const [selected, setSelected] = useState(currentTab)
     const onTabSelected = (tab: number) => {
         setSelected(tab)
         setCookie('selectedTab', tab.toString())
+        router.refresh()
     }
 
 
