86dc8b97426982ac4ffed2e3141df316326e3a97
Armando Garcia <armandobfmv@gmail.com>
2025-02-24 15:32:24 -0600

    clase 174 - información del usuario - Client side

---
+++ src/app/dashboard/profile/page.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+export default function ProfilePage() {
+
+    const {data: session } = useSession()
+
+    useEffect(() => {
+        console.log('client side')
+
+
+    })
+    return (
+        <div>
+            <h1>Profile Page</h1>
+
+            <hr />
+
+            <div className="flex flex-col">
+                <span>{session?.user?.name ?? 'No Name'}</span>
+                <span>{session?.user?.email ?? 'No Email'}</span>
+                <span>{session?.user?.image ?? 'No image'}</span>
+            </div>
+        </div>
+    );
+}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { CiLogout } from 'react-icons/ci';
 import { SidebarItem } from './SidebarItem';
-import { IoBasketOutline, IoCalendarOutline, IoCheckboxOutline, IoCodeWorkingOutline, IoListOutline } from 'react-icons/io5';
+import { IoBasketOutline, IoCalendarOutline, IoCheckboxOutline, IoCodeWorkingOutline, IoListOutline, IoPerson, IoPersonOutline } from 'react-icons/io5';
 import { auth } from '../../auth';
 import { redirect } from "next/navigation";
 
@@ -32,6 +32,11 @@ const menuItems = [
     icon: <IoBasketOutline />,
     title: 'Products',
     path: '/dashboard/products'
+  },
+  {
+    icon: <IoPersonOutline />,
+    title: 'User profile',
+    path: '/dashboard/profile'
   },
 ]
 
