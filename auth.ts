bd9e161ca35ca056a13fd1e5856cb107764667b7
Armando Garcia <armandobfmv@gmail.com>
2025-03-03 13:18:58 -0600

    clase 177 - Prisma Adapter - AuthJS

--- auth.ts
+++ auth.ts
@@ -1,7 +1,10 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth"
 import Github from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "./prisma";
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Github, GoogleProvider],
 })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { redirect } from 'next/navigation'
 import Image from 'next/image'
 
 export default function Home() {
+
+  redirect('/dashboard')
   return (
     <>
       <span className='text-5xl'>Hola Mundo</span>
